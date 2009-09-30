bc75a0faa59fd1b51a8b81cb097985aea377c2d4
a.kulikov <a.kulikov@831aa504-a137-11de-8e34-650dad57af36>
2009-09-30 18:06:36 +0000

    initial index and init files

---
+++ trunk/index.php
@@ -0,0 +1,17 @@
+<?php
+    /********************************************************
+    
+                 InTerra Blog Machine  DELUXE
+        
+        @author     Kulikov Alexey <a.kulikov@gmail.com>    
+        @copyright  essentialmind gmbh, vienna, austria
+        @link       http://code.google.com/p/interradeluxe/
+        
+                    This is Open Source, so 
+                 we do not guarantee anything!
+    
+     *******************************************************/
+    require_once('init.inc.php');
+
+
+?>
