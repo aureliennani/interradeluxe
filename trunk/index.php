97c809510f1e23d9838302de663c135b0cae249a
a.kulikov <a.kulikov@831aa504-a137-11de-8e34-650dad57af36>
2009-10-01 12:31:37 +0000

    Root COntroller Processing Model

--- trunk/index.php
+++ trunk/index.php
@@ -12,6 +12,60 @@
     
      *******************************************************/
     require_once('init.inc.php');
+    require_once($GLOBALS['_CONFIG']['root'].'classes/core/controller.class.php');
 
-
+    #### Prepare #######
+    #
+    //security audit
+    $_GET['controller'] = str_replace('/','',$_GET['controller']);
+    $_GET['controller'] = str_replace('.','',$_GET['controller']);
+    $_GET['controller'] = str_replace('\\','',$_GET['controller']);
+    $_GET['controller'] = str_replace(' ','',$_GET['controller']);
+    
+    //check for empty case
+    if(!$_GET['controller']){
+        if(!$_GET['admin']){
+            $_GET['controller'] = 'root';
+        }else{
+            $_GET['controller'] = 'adminBase';
+            $smarty->caching    = false;        //no cache for admin side
+        }
+    }       
+    
+    //check for existence (otherwise 404)
+    if(!file_exists($GLOBALS['_CONFIG']['root'].'classes/core/controller'.ucfirst($_GET['controller']).'.class.php')){
+        $_GET['controller'] = '404';
+        $cacheID = '404';
+    }else{        
+        //cache id
+        $cacheID = str_replace('/','|',substr(substr($_SERVER['REDIRECT_URL'],0,-1),1));
+    }
+    #
+    #### /Prepare ######
+    
+    ## This is the Heart and Sould ##
+    #
+    try{
+        #### Create ########
+        #
+        //call controller
+        $cName = 'controller'.ucfirst($_GET['controller']);
+        include_once($GLOBALS['_CONFIG']['root'].'classes/core/'.$cName.'.class.php');
+        $controller = new $cName($db, $smarty, $user);
+        #
+        #### /Create #######
+            
+        #### Display #######
+        #
+        //display the page ;)
+        $controller->display('index.html',$cacheID);
+        #
+        #### /Display ######
+        
+    }catch (exception $e){
+        echo $e->getMessage();
+        exit;
+    }
+    #
+    ## Yaaaa! ##
 ?>
