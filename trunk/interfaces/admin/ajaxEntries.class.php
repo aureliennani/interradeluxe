66813ab1c1ca576b89acdc3f89fc399a72b39271
a.kulikov <a.kulikov@831aa504-a137-11de-8e34-650dad57af36>
2009-10-08 19:43:21 +0000

    Created admin Interface

---
+++ trunk/interfaces/admin/ajaxEntries.class.php
@@ -0,0 +1,20 @@
+<?php
+    /********************************************************
+    
+                 InTerra Blog Machine  DELUXE
+        
+        @author     Kulikov Alexey <a.kulikov@gmail.com>        
+        @copyright  essentialmind gmbh, vienna, austria
+        @link       http://code.google.com/p/interradeluxe/
+        
+                    This is Open Source, so 
+                 we do not guarantee anything!
+    
+     *******************************************************/
+    class ajaxEntries extends ajax{
+
+        public function process(){              
+            echo 1;
+            exit;
+        }
+    }
